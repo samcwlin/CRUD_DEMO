e73a9d2a0a647d5d458eaae3ebb4faa788974354
Sam Lin <newaway.0@gmail.com>
2016-11-17 11:25:07 +0800

    加入專案檔案。

---
+++ CRUD_DEMO/CRUD_DEMO/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Unity;
+using CRUD_DEMO.Views;
+
+namespace CRUD_DEMO
+{
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
+
+			NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<MainPage>();
+		}
+	}
+}
